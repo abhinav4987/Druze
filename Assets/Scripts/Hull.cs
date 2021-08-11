0b8b7744dc58dab47862732b0a0b83f7baf37cef
abhinav4987 <abhinavanand4987@gmail.com>
2021-08-11 16:42:56 +0530

    Hull, sail and cannons integrated
    first Ship integration

--- Assets/Scripts/Hull.cs
+++ Assets/Scripts/Hull.cs
@@ -2,37 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Hull : MonoBehaviour
 {
-    float healthPoint;
-
+    private float healthPoint;
     
+    private float currentSpeed;
     float defence;
 
     int cannonColumnsCount = 2;
+    int cannonsPerColumn = 5;
 
+    Cannons[,] shipCannonColumn = new Cannons[cannonColumns, cannonsPerColumn];
 
-    Cannons[] shipCannonColumn = new Cannons[cannonColumns];
-
-    
     Sail ShipSail = new ShipSail();
 
+    Ram ShipRam = new Ram();
+
+    public float getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public float setCurrentSpeed(float newSpeed) {
+        currentSpeed = newSpeed;
+    }
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
+    public void setHealthPoint(int newHealthPoint) {
+        healthPoint = newHealthPoint;
+    }
+
+    public void setCannonAngle(int columnIndex, float angle) {
+        for(int i = 0; i < cannonsPerColumn;i++) {
+            shipCannonColumn[columnIndex][i].setAngle(angle); 
+        }
+    }
+
+    public void fireCannons(int column) {
+
+        for(int i =0;i< cannonsPerColumn;i++) {
+            hipCannonColumn[column][i].shoot();
+        }
+    }
 
 
-    void takeDmage(float damageIncurred) {
+    public void takeDmage(float damageIncurred) {
         health -= damageIncurred;
     }
 
-    void setCannonAngle(int cannonColumnIndex, float cannonAngle) {
+
+    public void rotateHull(float rotationAngle) {
 
     }
 
-    void fireCannon(int cannonColumnIndex) {
-
+    public float getSailSpeed() {
+        return ShipSail.getSailSpeed();
     }
 
-    void rotateHull(float rotationAngle) {
 
+    void UpdateCurrentSpeed () {
+        
+        if(ShipSail.getSailSpeed() != currentSpeed) {
+            float diff = currentSpeed - ShipSail.getSailSpeed();
+            if(diff == 1) {
+                setCurrentSpeed(ShipSail.getSailSpeed());
+            } else {
+                setCurrentSpeed(currentSpeed + diff/2);
+            }
+        }
     }
 
 
@@ -41,9 +81,10 @@ public class Hull : MonoBehaviour
         
     }
 
+
     void Update()
     {
-        
+        UpdateCurrentSpeed();
     }
 }
 
--- Assets/Scripts/Ram.cs
+++ Assets/Scripts/Ram.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Ram : MonoBehaviour
 {
     private float maxDamage;
