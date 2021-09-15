81c0650270efc0e5b92cab97b93eb9cea72acfff
abhinav4987 <abhinavanand4987@gmail.com>
2021-09-16 03:41:36 +0530

    Fire Cannon action
    Update in Fire Cannon action

--- Assets/Scripts/Tree Code/Nodes/Actions/FireCannon.cs
+++ Assets/Scripts/Tree Code/Nodes/Actions/FireCannon.cs
@@ -6,6 +6,20 @@ public class FireCannon : Node {
 
     public override NodeState Evaluate(ShipAIParameters shipParameters, ShipAIActionControls shipControls)
     {
+        Vector3 relativePoint = shipParameters.parameters.enemyTransform.InverseTransformPoint(0, 0, 0);
+
+        if (relativePoint.x > 0){
+            //ENEMY TOWARDS RIGHT;
+            shipControls.FireCannon();
+        } else if (relativePoint.x > 0){
+            // ENEMY INFRONT 
+            shipControls.FireCannon();
+        }
+        else{
+            // ENEMY TOWARDS LEFT
+            shipControls.FireCannon();
+        }
+
         shipControls.FireCannon();
         return NodeState.SUCCESS;
     }
